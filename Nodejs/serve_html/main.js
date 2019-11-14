ce5c948ebc4e2ecee596e54e5d05dee9a0498430
daikonus <daikonus_ksana@icloud.com>
2019-11-14 23:37:13 +0900

    Lesson6: complete

--- Nodejs/serve_html/main.js
+++ Nodejs/serve_html/main.js
@@ -1,30 +1,71 @@
 const port = 3000,
       http = require("http"),
-      httpStatus = require("http-status-codes"),
-      fs = require("fs");
-
-      const routeMap = {
-            "/": "views/index.html"
+      httpStatusCodes = require("http-status-codes"),
+      router = require("./router"),
+      fs = require("fs"),
+      plainTextContentType = {
+        "Content-Type": "text/plain"
+      },
+      htmlContentType = {
+        "Content-Type": "text/html"
+      },
+      cssContentType = {
+        "Content-Type": "text/css"
+      },
+      jsContentType = {
+        "Content-Type": "text/javascript"
+      },
+      imageContentType = {
+        "Content-Type": "image/jpeg"
+      },
+      customReadFile = (file, res) => {
+        fs.readFile(`./${file}`, (error, data) => {
+          if (error) {
+            console.log("Error reading the file...");
+          }
+          res.end(data);
+        });
       };
 
-const getViewUrl = (url) => {
-  return `views${url}.html`;
-};
+router.get("/", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, plainTextContentType);
+  res.end("INDEX");
+});
 
-http.createServer((req, res) => {
-  let viewUrl = getViewUrl(req.url);
-  fs.readFile(viewUrl, (error, data) => {
-    if (error) {
-      res.writeHead(httpStatus.NOT_FOUND);
-      res.write("<h1>FILE NOT FOUND</h1>");
-    } else {
-      res.writeHead(httpStatus.OK, {
-        "Content-Type": "text/html"
-      });
-      res.write(data);
-    }
-    res.end();
-  });
-})
-.listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
+router.get("/index.html", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, htmlContentType);
+  customReadFile("views/index.html", res);
+});
+
+router.get("/blog.html", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, htmlContentType);
+  customReadFile("views/blog.html", res);
+});
+
+router.get("/uikit-rtl.min.css", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, cssContentType);
+  customReadFile("public/css/uikit-rtl.min.css", res);
+});
+
+router.get("/uikit.min.js", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, jsContentType);
+  customReadFile("public/js/uikit.min.js", res);
+});
+
+router.get("/uikit-icons.min.js", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, jsContentType);
+  customReadFile("public/js/uikit-icons.min.js", res);
+});
+
+router.get("/images/wolf.jpg", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, imageContentType);
+  customReadFile("public/images/wolf.jpg", res);
+});
+
+router.post("/", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, plainTextContentType);
+  res.end("POSTED");
+});
+
+http.createServer(router.handle).listen(port);
+console.log(`The server is listening on port number: ${port}`);
