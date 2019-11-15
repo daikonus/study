e9c7e06fb5b6cd44fde8df386d6e2238b1ec075c
fj3176if <fj3176if@aa.jp.fujitsu.com>
2019-11-15 13:23:02 +0900

    Lesson7: create router module, utils module, contentType modele

---
+++ Nodejs/confetti_cuisine/main.js
@@ -0,0 +1,6 @@
+const port = 3000,
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  router = require("./router"),
+  contentTypes = require("./contentTypes"),
+  utils = require("./utils");
