deccff25aaaea0c02661454aa4734e44e4046d41
daikonus <daikonus_ksana@icloud.com>
2019-11-19 22:16:32 +0900

    commit

--- Nodejs/unit2/express_templates/controllers/homeController.js
+++ Nodejs/unit2/express_templates/controllers/homeController.js
@@ -1,3 +1,4 @@
 exports.respondWithName = (req, res) => {
-  res.render("index");
+  let paramsName = req.params.myName;
+  res.render("index", { name: paramsName});
 };
--- Nodejs/unit2/express_templates/main.js
+++ Nodejs/unit2/express_templates/main.js
@@ -1,5 +1,6 @@
 const port = 3000,
   express = require("express"),
+  layouts = require("express-ejs-layouts"),
   app = express(),
   homeController = require("./controllers/homeController");
 
@@ -7,3 +8,5 @@ app.set("view engine", "ejs");
 app.listen(port, () => {
   console.log(`Server runing on port number: ${port}`);
 });
+
+app.get("/name/:myName", homeController.respondWithName);
