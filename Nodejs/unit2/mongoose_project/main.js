3aafc21a65e9424af85a38a0507bc8ff9fb67ca3
daikonus <daikonus_ksana@icloud.com>
2019-11-24 23:46:00 +0900

    Lesson15: complete. promice,then,catch用法。いやそれ以前にejsのpatternの{}抜けとか、コールバック関数の引数漏れとかキツいっス。

--- Nodejs/unit2/mongoose_project/main.js
+++ Nodejs/unit2/mongoose_project/main.js
@@ -1,25 +1,49 @@
+const express = require("express"),
+  app = express(),
+    layouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-
+const subscribersController = require("./controllers/subscribersController");
+const db = mongoose.connection;
+// const subscriberSchema = mongoose.Schema({
+//   // <property>: <data type>,
+//   name: String,
+//   email: String,
+//   zipCode: Number
+// });
 mongoose.connect(
   "mongodb://localhost:27017/recipe_db",
   {useNewUrlParser: true}
 );
-
-const db = mongoose.connection;
-
 db.once("open", () => {
   console.log("Successfully connected to MongoDB using Mongoose!");
 });
+mongoose.Promise = global.Promise
 
-const subscriberSchema = mongoose.Schema({
-  // <property>: <data type>,
-  name: String,
-  email: String,
-  zipCode: Number
+// HTTPリクエストを３０００ポートで受け付ける
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get("port"), () => {
+  console.log(
+    `Server running at http://localhost:${app.get("port")}`
+  );
+});
+
+app.set("view engine", "ejs");
+app.use(layouts);
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+
+app.get("/contact", subscribersController.getSubscriptionPage);
+app.post("/subscribe", subscribersController.saveSubscriber);
+app.get("/subscribers", subscribersController.getAllSubscribers, (req, res, next) => {
+  console.log(req.data);
 });
 
 // const Subscriber = mongoose.model("Subscriber", subscriberSchema)
-
+//
 // var subscriber1 = new Subscriber({
 //   name: "Jon Wexler",
 //   email: "jon@jonwexler.com"
@@ -41,15 +65,15 @@ const subscriberSchema = mongoose.Schema({
 //   }
 // );
 
-const Subscriber = require("./models/subscriber")
-
-Subscriber.findOne({ name: "Jon Wexler" }).where("email", /wexler/);
-
-var myQuery = Subscriber.findOne({
-  name: "Jon Wexler"
-  })
-  .where("email", /wexler/);
-
-myQuery.exec((error, data) => {
-  if (data) console.log(data.name);
-});
+// const Subscriber = require("./models/subscriber")
+//
+// Subscriber.findOne({ name: "Jon Wexler" }).where("email", /wexler/);
+//
+// var myQuery = Subscriber.findOne({
+//   name: "Jon Wexler"
+//   })
+//   .where("email", /wexler/);
+//
+// myQuery.exec((error, data) => {
+//   if (data) console.log(data.name);
+// });
