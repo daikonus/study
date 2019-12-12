23b995bc98016ca362b3f8d3b15edf8c9efdcb72
fj3176if <fj3176if@aa.jp.fujitsu.com>
2019-12-12 09:35:51 +0900

    modify get_log.sh

--- Shell/get_log.sh
+++ Shell/get_log.sh
@@ -1,24 +1,24 @@
 #!/bin/sh -eu
 
 # 一括ダウンロード
-aws s3 cp s3://gazelle-stg-cloudfront-fighters-ad/ ./tmp/ --exclude "*" --include "*2019-12-05*" --recursive
+aws s3 cp s3://XXXX/ ./tmp/ --exclude "*" --include "*2019-12-05*" --recursive
 
 # CloudFrontの場合(APサーバ側)
-aws s3 ls s3://gazelle-stg-cloudfront-fighters-ad | grep 2019-12-05 | awk '{print $4}' > filenamelist
+aws s3 ls s3://XXXX | grep 2019-12-05 | awk '{print $4}' > filenamelist
 # CloudFrontの場合(バックエンド静的コンテンツバケット)
-aws s3 ls s3://gazelle-stg-cloudfront-fighters-ad-stat | grep 2019-11-18-00 | awk '{print $4}' > filenamelist
+aws s3 ls s3://XXXX-stat | grep 2019-11-18-00 | awk '{print $4}' > filenamelist
 # ALBの場合
-aws s3 ls s3://gazelle-stg-elb-fighters-ad/AWSLogs/098435297579/elasticloadbalancing/ap-northeast-1/2019/11/18/ | awk '{print $4}' > filenamelist
+aws s3 ls s3://XXXX/AWSLogs/098435297579/elasticloadbalancing/ap-northeast-1/2019/11/18/ | awk '{print $4}' > filenamelist
 # istio-proxyポッドの場合
-aws s3 ls s3://gazelle-stg-applog-bk/2019-11-18/fighters/istio-proxy/ | awk '{print $4}' > filenamelist
+aws s3 ls s3://XXXX/2019-11-18/fighters/istio-proxy/ | awk '{print $4}' > filenamelist
 # backポッド(通常)の場合
-aws s3 ls s3://gazelle-stg-applog-bk/2019-11-18/fighters/back/ | awk '{print $4}' | grep -v GC > filenamelist
+aws s3 ls s3://XXXX/2019-11-18/fighters/back/ | awk '{print $4}' | grep -v GC > filenamelist
 # backポッド(GCログ)の場合
-aws s3 ls s3://gazelle-stg-applog-bk/2019-11-18/fighters/back/ | awk '{print $4}' | grep GC > filenamelist
+aws s3 ls s3://XXXX/2019-11-18/fighters/back/ | awk '{print $4}' | grep GC > filenamelist
 
 
-# BucketName="gazelle-stg-elb-fighters-ad/AWSLogs/098435297579/elasticloadbalancing/ap-northeast-1/2019/11/18"
-BucketName="gazelle-stg-cloudfront-fighters-ad"
+# BucketName="XXXX/AWSLogs/098435297579/elasticloadbalancing/ap-northeast-1/2019/11/18"
+BucketName="XXXX"
 
 for zipfile in `cat filenamelist`;do
   # ログファイル取得
